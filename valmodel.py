99319ec56c612cad99f886acb49ceb05a38234a8
UltralyticsAssistant <web@ultralytics.com>
2026-06-08 04:20:17 +0000

    Auto-format by https://ultralytics.com/actions

--- valmodel.py
+++ valmodel.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
+
 def main():
     model = YOLO("yologoldP2v0.pt")
 
@@ -9,11 +10,12 @@ def main():
         batch=16,
         imgsz=640,
         device=0,
-        workers=0  # tránh lỗi multiprocessing trên Windows
+        workers=0,  # tránh lỗi multiprocessing trên Windows
     )
 
     print(f"mAP50: {metrics.box.map50:.4f}")
     print(f"mAP50-95: {metrics.box.map:.4f}")
+
 
 if __name__ == "__main__":
     main()
