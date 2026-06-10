60267e4400ab5fc5f4c0d375076e451e5b812f7d
Dang Van Nguyen <NguyenDang121504@gmail.com>
2026-06-10 21:17:47 +0700

    WIoU - CIoU

--- valmodel.py
+++ valmodel.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yologoldP2v0.pt")
+    model = YOLO("yologoldP2SimSPPF.pt")
 
     metrics = model.val(
         data="C:\\Users\\Nguyen\\Final_report\\YOLO-APD\\data\\data.yaml",
